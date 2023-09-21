cd95db7d4bc7517e4ccc82ab9bc9e0faa98a1b8f
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-09-21 20:04:12 +0530

    Refactored Librarian - Removed repeating code

--- src/services/librarian.py
+++ src/services/librarian.py
@@ -4,9 +4,8 @@ class Librarian:
     def __init__(self):
         pass
 
-    def title_tracker(self, path_to_search, string_to_match) -> list:
+    def __exec(self, command: str) -> list:
         try:
-            command = f"find {path_to_search} -type f -name '*{string_to_match}*'"
             result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             if result.returncode != 0:
@@ -18,22 +17,15 @@ class Librarian:
         
         except Exception as e:
             return str(e)
+
+    def title_tracker(self, path_to_search, string_to_match) -> list:
+        command = f"find {path_to_search} -type f -name '*{string_to_match}*'"
+        return self.__exec(command)
 
     def content_curator(self, path_to_search, string_to_match) -> list:
-        try:
-            command = f"find {path_to_search} -type f -exec grep -li '{string_to_match}' {{}} +"
-            result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            if result.returncode != 0:
-                raise Exception(result.stderr)
-            
-            filenames = result.stdout.splitlines()
-            filepaths = list(map(os.path.normpath, filenames))
-            return filepaths
-        
-        except Exception as e:
-            return str(e)
+        command = f"find {path_to_search} -type f -exec grep -li '{string_to_match}' {{}} +"
+        return self.__exec(command)
 
 
 if __name__ == '__main__':
-    print(Librarian().title_tracker('/Users/saichitt/Suman//', 'pirate'))
+    print(Librarian().title_tracker('/home/suman/', 'pirate'))
