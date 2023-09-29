453c5cf80deb7a0696930ca688c219297461e2da
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-09-29 22:24:12 +0530

    Updated boilerplate for Jarvis Root Application

--- Jarvis.py
+++ Jarvis.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+from src.integrations.discord import app as discord_app
+
 class Jarvis:
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    discord_app.start()
