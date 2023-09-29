6fa969d4d59c7870a2ba50f2c69eb117fb0e8479
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-09-29 19:33:59 +0530

    Improved readability of Torrent flow

--- src/flows/Pirate_Flows.py
+++ src/flows/Pirate_Flows.py
@@ -1,24 +1,42 @@
+#!/usr/bin/python3
+
 import collections
+import logging
+
+import discord, time
 
 from src.common.Flow import Flow
+from src.services.informio import Informio
 from src.services.pirate import Pirate
+from src.common.response import Response
 
 
-class MagnetDownload(Flow):
+class Torrent(Flow):
     """
-    Downloads a torrent file from a magnet URL.
+    Handles various routines of BitTorrent
+    based on the task and additional arguments.
 
     Trigger:
-        !magnet
+        !torrent
 
     Execution Args:
-        magnet_url (str): The magnet URL of the torrent to download.
+        task (str): {"list", "add", "remove", "start", "pause"}
+        Additional Args:
+            "list":
+                None
+            "add":
+                "url": {"magnet_url", "http_url"} or
+                "attachment": {"torrent_file"}
+            "remove":
+                "id": {"torrent_id"}
+            "start":
+                "id": {"torrent_id"}
+            "pause":
+                "id": {"torrent_id"}
 
     Returns:
-        bool: True if the torrent was successfully downloaded, False otherwise.
-
-    Raises:
-        ValueError: If the magnet URL is invalid or empty.
+        Response based on the task chosen. If args mismatch, a message
+        representing the issue will be returned.
 
     Notes:
         This function requires a BitTorrent client to be installed on the system,
@@ -28,78 +46,113 @@ class MagnetDownload(Flow):
         in some countries. Please ensure that you comply with the applicable laws
         and regulations of your jurisdiction.
 
-    Example magnet url: "magnet:?xt=urn:btih:c8bb8c7e50bbae9bc9599639e835c0117f0c7e3d&dn=example.torrent"
+    Example Args:
+        1.
+            !pirate
+            task = list
+        2.
+            !pirate
+            task = add
+            url = magnet:?xt=urn:btih:c8bb8c7e50bbae9bc9599639e835c0117f0c7e3d&dn=example.torrent
+        3.
+            !pirate
+            task = add
+            url = https://<torrent_link>.torrent
+        4.
+            !pirate
+            task = add
+            # Add an attachment
+        5.
+            !pirate         !pirate         !pirate
+            task = stop     task = start    task = pause
+            id = 1          id = 2          id = 4
+
     """
 
     traces = []
 
     @classmethod
     def trigger(cls) -> str:
-        return '!magnet'
+        return '!torrent'
 
     def exec(self, args: collections.defaultdict) -> dict:
-        magnet_url = args.get('magnet_url')
         client = Pirate()
-        response = client.add(magnet_url)
-        self.traces.append(response)
-        return {
-            'response': response
-        }
 
-    @classmethod
-    def ps(cls) -> list:
-        return cls.traces
-
-    @classmethod
-    def purge(cls) -> bool:
         try:
-            cls.traces.clear()
-            return True
-        except:
-            return False
+            task = args.get('task')
+
+            if task == 'list':
+                if len(args) > 1:
+                    raise ValueError('Got Invalid number of Arguments')
+                resp = client.list()
+
+            elif task == 'add':
+                if 'torrent' in args or 'url' in args:
+                    if len(args) > 2:
+                        raise ValueError('Got Invalid number of Arguments')
+                    if 'url' in args:
+                        torrent = args.get('url')
+                    else:
+                        torrent = args.get('torrent')
+                    resp = client.add(torrent)
+
+                else:
+                    raise ValueError(f'Missing argument/attachment for task {task}')
+
+            elif task in ['remove', 'start', 'pause']:
+                if 'id' not in args:
+                    raise ValueError(f'Id not provided for task {task}')
+                elif len(args) > 2:
+                    raise ValueError('Got Invalid number of Arguments')
+                else:
+                    torrent_id = int(args.get('id'))
+                    resp = client.__getattribute__(task)(torrent_id)
+
+            else:
+                resp = {
+                    'status': 'error',
+                    'message': f'Invalid task: {task}'
+                }
+
+        except Exception as e:
+            resp = {
+                'status': 'error',
+                'message': str(e)
+            }
+
+        return resp
+    
+    async def capture_discord(self, args: collections.defaultdict, message: discord.Message, informio: Informio):
+        acknowledgement = Response('success', f'{self.trigger()} request has been captured. Please wait!')
+        informio.send_message(str(acknowledgement))
+
+        time.sleep(1)
+
+        if message.attachments:
+            torrent = await message.attachments[0].read()
+            args['torrent'] = torrent
+
+        resp = self.exec(args)
+
+        await self.respond_discord(resp, message, informio)
 
 
-class TorrentFromHTTP(Flow):
-    """
-    Downloads a torrent file from a HTTP URL.
+    async def respond_discord(self, resp: collections.defaultdict, message: discord.Message, informio: Informio):
+        
+        if resp['status'] == 'success':
+            response = Response('success', 'Your moment of anticipation is over. Here ya go!')
+            response += Response('info', resp['message'])
 
-    Trigger:
-        !torrent_http
+        else:
+            response_text = "It appears we've encountered an unexpected problem!\n"
+            response_text += '\n'.join(
+                [
+                    f'{key}: {value}' for key, value in resp.items()
+                ]
+            )
+            response = Response('error', response_text)
 
-    Execution Args:
-        torrent_url (str): The HTTP URL pointing to the torrent file.
-
-    Returns:
-        bool: True if the torrent was successfully downloaded, False otherwise.
-
-    Raises:
-        ValueError: If the URL is invalid or empty.
-
-    Notes:
-        This function requires a BitTorrent client to be installed on the system,
-        such as uTorrent, qBittorrent, or Transmission.
-
-        It is important to note that downloading copyrighted material may be illegal
-        in some countries. Please ensure that you comply with the applicable laws
-        and regulations of your jurisdiction.
-
-    Example URL: "http://example.com/example.torrent"
-    """
-
-    traces = []
-
-    @classmethod
-    def trigger(cls) -> str:
-        return '!torrent_http'
-
-    def exec(self, args: collections.defaultdict) -> dict:
-        url = args.get('torrent_url')
-        client = Pirate()
-        response = client.add(url)
-        self.traces.append(response)
-        return {
-            'response': response
-        }
+        await message.reply(str(response))
 
     @classmethod
     def ps(cls) -> list:
