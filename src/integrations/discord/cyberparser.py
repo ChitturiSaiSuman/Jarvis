0e34d2187e9cf38d140d9385c1f996e52aa5b9df
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-09-29 19:38:07 +0530

    Implemented Message Parser using Cyber Parser

--- src/integrations/discord/cyberparser.py
+++ src/integrations/discord/cyberparser.py
@@ -1,4 +1,5 @@
-import collections
+#!/usr/bin/python3
+
 import json
 import re
 
@@ -6,63 +7,66 @@ from src.common.trigger_loader import TriggerLoader
 
 
 class CyberParser:
-    class Message:
-        def __init__(self):
-            pass
 
     def __init__(self, trigger_loader: TriggerLoader):
         self.utils = set(trigger_loader.utils)
         
-
-    def parse(self, message: str) -> Message:
+    def parse(self, message: str) -> dict:
         message = message.strip()
-        if not message:
-            # TODO
-            signature = CyberParser.Message()
-            signature.kind = 'empty'
-            return signature
+        signature = dict()
 
-        if message in self.utils:
-            signature = CyberParser.Message()
-            signature.kind = 'util'
-            signature.command = message
+        if not message:
+            signature['kind'] = 'empty'
+            signature['message'] = 'Got an Empty Message'
+
+        elif message in self.utils:
+            signature['kind'] = 'util'
+            signature['command'] = message
         
         elif message.startswith('!'):
             signature = self.__parse_flow_request(message)
             
         else:
-            signature = CyberParser.Message()
-            signature.kind = 'general'
-            signature.message = message
-            
+            signature['kind'] = 'general'
+            signature['message'] = message
 
-    def __parse_flow_request(self, message: str):
+        return signature
+
+    def __parse_flow_request(self, message: str) -> dict:
         command, content = re.split(r'\s+', message.strip(), 1)
+        signature = dict()
+
         if content.startswith('{') and content.endswith('}'):
             try:
                 json_body = json.loads(content)
-                signature = CyberParser.Message()
-                signature.kind = 'flow'
-                signature.command = command
-                signature.args = json_body
-                return signature
+                signature['king'] = 'flow'
+                signature['command'] = command
+                signature['args'] = json_body
             
             except json.JSONDecodeError as e:
-                signature = CyberParser.Message()
-                signature.kind = 'error'
-                signature.message = str(e)
-                return signature
+                signature['kind'] = 'error'
+                signature['message'] = str(e)
+                
         else:
-            pairs = re.findall(r'(\w+)\s*[:=]\s*([^=\n]+)', content)
+            content_lines = content.splitlines()
+            pairs = []
+            for line in content_lines:
+                match = re.match(r'(\w+)\s*[:=]\s*(.*)', line)
+                if match:
+                    key, value = match.groups()
+                    pairs.append((key, value))
+                else:
+                    pairs = []
+                    break
+
             if pairs:
-                body = collections.defaultdict(pairs)
-                signature = CyberParser.Message()
-                signature.kind = 'flow'
-                signature.command = command
-                signature.args = body
-                return signature
+                body = dict(pairs)
+                signature['kind'] = 'flow'
+                signature['command'] = command
+                signature['args'] = body
             
             else:
-                signature = CyberParser.Message()
-                signature.kind = 'error'
-                signature.message = 'Unable to decode pairs'
+                signature['kind'] = 'error'
+                signature['message'] = 'Unable to decode pairs'
+
+        return signature
