d3fa347e08e93ae7906479f1b47143e33ab33c2a
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-10-02 17:49:34 +0530

    Added logging stuff

--- Jarvis.py
+++ Jarvis.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
 from src.integrations.discord import app as discord_app
+import logging
 
 class Jarvis:
     def __init__(self):
         pass
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+        datefmt='%Y-%m-%d:%H:%M:%S',
+        level=logging.INFO,
+        filename='log.out',
+        filemode='w'
+    )
     discord_app.start()
