60e8eb7304d7294c2f1125bd7028d6892a470e5d
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-12-03 11:49:35 +0530

    Added setup information and updated README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Jarvis
 Everything you need in a Machine
+
+<details open>
+<summary>Setup</summary>
+
+* Clone the Repository
+
+    ```shell
+    git clone https://www.github.com/ChitturiSaiSuman/Jarvis
+    ```
+* Navigate to Jarvis
+    ```shell
+    cd Jarvis
+    ```
+
+* Install all dependencies
+    ```shell
+    sudo bash setup.sh
+    ```
+</details>
--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
 # Install pip
-sudo apt install python3-pip
+    sudo apt install python3-pip
 
 # Install system utilities
-sudo apt install nmap wireless-tools
-
-# Install Python packages
-sudo pip3 install python-nmap psutil discord.py discord_webhook google-auth google-api-python-client pyzipper
+    sudo apt install nmap wireless-tools
 
 # Install Transmission Daemon, a BitTorrent client
-sudo apt-get install transmission-daemon
-sudo pip3 install transmission-rpc
+    sudo apt-get install transmission-daemon
 
 # Stop Transmission Daemon to edit configuration
-sudo service transmission-daemon stop
+    sudo service transmission-daemon stop
 
 # Edit the Transmission Daemon configuration file
-sudo sed -i 's/"rpc-whitelist-enabled": true,/"rpc-whitelist-enabled": false,/' /etc/transmission-daemon/settings.json
+    sudo sed -i 's/"rpc-whitelist-enabled": true,/"rpc-whitelist-enabled": false,/' /etc/transmission-daemon/settings.json
 
 # Restart Transmission Daemon
-sudo service transmission-daemon start
+    sudo service transmission-daemon start
+
+# Install Python packages
+    sudo pip3 install -r requirements.txt
