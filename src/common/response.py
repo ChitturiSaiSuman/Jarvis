10ec370d7ed3dc494760142abf3212fcfdf4dae4
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2024-02-17 20:28:29 +0530

    Added a method to concatenate two responses

--- src/common/response.py
+++ src/common/response.py
@@ -11,8 +11,6 @@ class Response:
         return config.Constants.response
 
     def __init__(self, response_type: str, response: str):
-        if not response.endswith('\n'):
-            response += '\n'
         self.raw = response
         self.__resp = self.load()
         artist = artificer.Artificer(self.__resp[response_type], self.raw)
@@ -21,6 +19,15 @@ class Response:
 
     def __str__(self) -> str:
         return f"```ansi\n{self.art}```"
+    
+    def add(self, other_response, sep='\n'):
+        if isinstance(other_response, Response):
+            self.art += sep + other_response.art
+            return self
+        elif isinstance(other_response, str):
+            return Response(self.resp_type, self.raw + other_response)
+        else:
+            raise TypeError('Unsupported operand for +')
 
     def __add__(self, other_response):
         if isinstance(other_response, Response):
