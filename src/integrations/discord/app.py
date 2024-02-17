0f0393d5fc42b4bc8929c9ada71e7ec8db9a0bcc
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2024-02-17 20:29:35 +0530

    modified __send_welcome() method to handle dynamic messages

--- src/integrations/discord/app.py
+++ src/integrations/discord/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+import functools
+
+import src.integrations.discord.bot as bot
 from src.common.config import Constants
 from src.common.response import Response
 from src.common.trigger_loader import TriggerLoader
-import src.integrations.discord.bot as bot
 from src.services.informio import Informio
 
 
@@ -18,13 +20,30 @@ def __send_welcome():
 
     states_and_messages = list(map(trigger_loader.fetch, triggers))
 
-    information_for_user = '\n'.join([
-        f'{message}{state.job()}' for state, message in states_and_messages
-    ])
+    responses = [
+        (message, state.job()) for state, message in states_and_messages
+    ]
+
+    responses = map(
+        lambda response: (
+            response[0],
+            'error' if response[1]['status'] == 'error' else 'general',
+            response[1]['message']
+        ),
+        responses
+    )
+
+    status = functools.reduce(
+        lambda resp1, resp2: resp1.add(resp2),
+        [Response('general', triplet[0]) + Response(triplet[1], str(triplet[2])) for triplet in responses]
+    )
+
+    print(status)
 
     welcome_message = Response('success', greeting + '\n')
     welcome_message += Response('info', comment + '\n')
-    welcome_message += Response('general', information_for_user)
+
+    welcome_message += status
 
     Informio().send_message(str(welcome_message))
 
